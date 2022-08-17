2d8119d20964b2462dab32457bc93cc24b32787e
BigNinjaChicken <50557956+BigNinjaChicken@users.noreply.github.com>
2022-08-16 22:30:04 -0400

    Attept

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-console.log("JavaScript rules!")
+/*
+const { EventEmitter } = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('lunch', () => console.log('yum'))
+
+eventEmitter.emit('lunch');
+eventEmitter.emit('lunch');
+*/
+
+// Snyc === Blocking
+/*
+const { readFile, readFileSync } = require('fs');
+
+const txt = readFileSync('./hello.txt', 'utf8');
+
+console.log(txt);
+
+console.log('do this ASAP');
+*/
+
+const cheerio = require('cheerio');
+const getUrls = require('get-urls');
+const fetch = require('node-fetch');
+
+const scrapeMetatags = (text) => {
+
+
+
+    const urls = Array.from( getUrls(text) );
+
+    const requests = urls.map(async url => {
+
+        const res = await fetch(url);
+
+        const html = await res.text();
+        const $ = cheerio.load(html);
+        
+        const getMetatag = (name) =>  
+            $(`meta[name=${name}]`).attr('content') ||  
+            $(`meta[name="og:${name}"]`).attr('content') ||  
+            $(`meta[name="twitter:${name}"]`).attr('content');
+
+        return { 
+            url,
+            title: $('title').first().text(),
+            favicon: $('link[rel="shortcut icon"]').attr('href'),
+            // description: $('meta[name=description]').attr('content'),
+            description: getMetatag('description'),
+            image: getMetatag('image'),
+            author: getMetatag('author'),
+        }
+    });
+
+
+    return Promise.all(requests);
+
+}
